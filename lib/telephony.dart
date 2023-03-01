eac82e5cd785f09945681b45ff9cfa3fc8fc8c42
Ali Kamal <alikamaltaki@gmail.com>
2023-03-01 12:15:30 +0300

    Update telephony.dart

--- lib/telephony.dart
+++ lib/telephony.dart
@@ -12,6 +12,7 @@ part 'filter.dart';
 typedef MessageHandler(SmsMessage message);
 typedef SmsSendStatusListener(SendStatus status);
 
+@pragma('vm:entry-point')
 void _flutterSmsSetupBackgroundChannel(
     {MethodChannel backgroundChannel =
         const MethodChannel(_BACKGROUND_CHANNEL)}) async {
